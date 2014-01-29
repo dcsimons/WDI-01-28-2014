38316edc542f91ba5abbe541610cb0ff6438abe8
David Simons <dave.c.simons@gmail.com>
2014-01-28 17:12:26 -0800

    functional store app

--- store/config/routes.rb
+++ store/config/routes.rb
@@ -1,9 +1,11 @@
 Store::Application.routes.draw do
 
-  root "categories#index"
+  root "categories#home"
 
   get "/categories", to: "categories#index", as: "categories"
   get "/categories/:id", to: "categories#show", as: "category"
 
+  get "/products", to: "products#index", as: "products"
+  get "/products/:id", to: "products#show", as: "product"
 
 end
